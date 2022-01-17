92e27add3ccdfa62c3cb8ca5b1ba6a44b16e800a
Ammar Halees <ammar.alhalees@maf.ae>
2022-01-17 16:54:17 +0200

    basics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Footer from "../components/custom-comp/Footer/Footer";
 import Header from "../components/custom-comp/Header/Header";
