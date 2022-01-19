5a93b0226cf8dc9e96227990c075569649896107
Ammar Halees <ammar.alhalees@maf.ae>
2022-01-19 18:42:38 +0200

    filteration

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,20 +4,57 @@ import axios from "axios";
 import { formatStringDataToArray } from "../utils/helpers";
 import { Hotel, Hotels } from "../utils/types";
 import HotelCard from "../components/custom-comp/HotelCard/HotelCard";
+import { useEffect, useState } from "react";
 
-export default function search({ results }: { results: string }) {
-  // function filterHotelsArrayBasedOnActiveFilters(formattedResults) {
-
-  // }
+export default function Search({ results }: { results: string }) {
+  // Sort:
+  function handleSort(sort: string) {
+    console.log(sort);
+  }
+  // Query
+  const [query, setQuery] = useState("");
+  function handleQueryChange({
+    target: { value: query },
+  }: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(query);
+  }
+  // Format API response
   const formattedResults: Hotels = formatStringDataToArray(results);
-  // const filteredArray =
-  //   filterHotelsArrayBasedOnActiveFilters(formattedResults);
+  const [localData, setLocalData] = useState(formattedResults);
+
+  //respond to change in query
+  useEffect(() => {
+    if (query.length > 0) {
+      const queryFilteredResults = localData.filter((hotel) =>
+        hotel.name.includes(query)
+      );
+      setLocalData(queryFilteredResults);
+    } else {
+      setLocalData(localData);
+    }
+  }, [query]);
+
+  //respond to change in price filter
+  // useEffect(() => {
+  //   if (query.length > 0) {
+  //   }
+  // }, [query]);
+
+  console.log(localData);
 
   return (
     <div className={styles.container}>
-      <aside className={styles.aside}>aside</aside>
+      <aside className={styles.aside}>
+        <input name="query" onChange={handleQueryChange} />
+      </aside>
       <div className={styles.results}>
-        <header>filters</header>
+        <header className={styles.header}>
+          <strong>Total Nights</strong>
+          <div className={styles.buttonContainer}>
+            <button onClick={() => handleSort("price")}>Sort by price</button>
+            <button onClick={() => handleSort("price")}>Sort by name</button>
+          </div>
+        </header>
 
         <main className={styles.main}>
           {formattedResults.map(
