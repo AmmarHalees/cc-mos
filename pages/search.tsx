b9933e57532cc6ff1a81c57a5333e2748e3e6b5e
Ammar Halees <ammar.alhalees@maf.ae>
2022-01-19 11:15:56 +0200

    search

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,3 +1,14 @@
+import styles from "../styles/Search.module.scss";
+
 export default function search() {
-  return <div>Search</div>;
+  return (
+    <div className={styles.container}>
+      <aside className={styles.aside}>aside</aside>
+      <div className={styles.results}>
+        <header>filters</header>
+
+        <main className={styles.main}>main</main>
+      </div>
+    </div>
+  );
 }
