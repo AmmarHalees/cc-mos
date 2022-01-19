a8f8031c3bacfa65949c365e464a6ee6dd2ced86
Ammar Halees <ammar.alhalees@maf.ae>
2022-01-19 13:16:00 +0200

    fecthing

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,10 @@
 import styles from "../styles/Search.module.scss";
+import { GetServerSideProps } from "next";
+import axios from "axios";
 
-export default function search() {
+export default function search({ results }: { results: string }) {
+  console.log(results.split(" ").join().split("\n"));
+
   return (
     <div className={styles.container}>
       <aside className={styles.aside}>aside</aside>
@@ -12,3 +16,12 @@ export default function search() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await axios.get(`${process.env.API_URL}`);
+  let results = res.data;
+
+  return {
+    props: { results }, // will be passed to the page component as props
+  };
+};
